90f0a876e00f61abad292876b31c31168ff49f7c
Tiago <tsc2@cin.ufpe.br>
2020-09-08 14:13:47 -0300

    adicionando dois graficos na paraiba

--- superset/views/extra.py
+++ superset/views/extra.py
@@ -37,66 +37,129 @@ def boletim_covid():
 @talisman(force_https=False)
 @app.route("/trend_paraiba_pcn")
 def trend_paraiba_pcn():
-   connection = psycopg2.connect('postgresql://' + USUARIO_BD + ':' + SENHA_BD + '@192.168.0.100:5433/target_pb')
-   cursor = connection.cursor()
-   cursor.execute('''select data as data_casos, "casosNovos" as casos_novos, cast(avg("casosNovos") over (order by data rows between 6 preceding and current row) as numeric(12,2)) pcn 
-   from "SES_PB" group by data, "casosNovos" order by data''')
-   
-   result = cursor.fetchall()
-   panda_query = pd.DataFrame(list(result))
-   
-   data = [
-       go.Bar(
-           x=panda_query[0],
-           y=panda_query[1],
-           name='Casos por dia',
-           hovertemplate='Data: %{x}<br>Casos confirmados: %{y}'           
-        ),
-       go.Scatter(
-           x=panda_query[0],
-           y=panda_query[2],
-           name='Media móvel'
-       )
-   ]
-   layout = go.Layout(
-           xaxis=go.layout.XAxis(tickmode='auto')
-      
-   )
-   fig = go.Figure(data=data,layout=layout)
-   pagina = io.to_html(fig)
-   cursor.close()
-   connection.close()
-   return pagina
+    connection = psycopg2.connect('postgresql://' + USUARIO_BD + ':' + SENHA_BD + '@192.168.0.100:5433/target_pb')
+    cursor = connection.cursor()
+    cursor.execute('''select data as data_casos, "casosNovos" as casos_novos, cast(avg("casosNovos") over (order by data rows between 6 preceding and current row) as numeric(12,2)) pcn 
+    from "SES_PB" group by data, "casosNovos" order by data''')
+    
+    result = cursor.fetchall()
+    panda_query = pd.DataFrame(list(result))
+    
+    data = [
+        go.Bar(
+            x=panda_query[0],
+            y=panda_query[1],
+            name='Casos por dia',
+            hovertemplate='Data: %{x}<br>Casos confirmados: %{y}'           
+         ),
+        go.Scatter(
+            x=panda_query[0],
+            y=panda_query[2],
+            name='Media móvel'
+        )
+    ]
+    layout = go.Layout(
+            xaxis=go.layout.XAxis(tickmode='auto')
+       
+    )
+    fig = go.Figure(data=data,layout=layout)
+    pagina = io.to_html(fig)
+    cursor.close()
+    connection.close()
+    return pagina
 
 @app.route("/trend_paraiba_obitos")
 def trend_paraiba_obitos():
-   connection = psycopg2.connect('postgresql://' + USUARIO_BD + ':' + SENHA_BD + '@192.168.0.100:5433/target_pb')
-   cursor = connection.cursor()
-   cursor.execute('''select data as data_casos, "obitosNovos" as obitos_novos, cast(avg("obitosNovos") over (order by data rows between 6 preceding and current row) as numeric(12,2)) pcn 
-   from "SES_PB" group by data, "obitosNovos" order by data''')
-   
-   result = cursor.fetchall()
-   panda_query = pd.DataFrame(list(result))
-   
-   data = [
-       go.Bar(
-           x=panda_query[0],
-           y=panda_query[1],
-           name='Obitos por dia',
-           hovertemplate='Data: %{x}<br>Obitos: %{y}'           
+    connection = psycopg2.connect('postgresql://' + USUARIO_BD + ':' + SENHA_BD + '@192.168.0.100:5433/target_pb')
+    cursor = connection.cursor()
+    cursor.execute('''select data as data_casos, "obitosNovos" as obitos_novos, cast(avg("obitosNovos") over (order by data rows between 6 preceding and current row) as numeric(12,2)) pcn 
+    from "SES_PB" group by data, "obitosNovos" order by data''')
+    
+    result = cursor.fetchall()
+    panda_query = pd.DataFrame(list(result))
+    
+    data = [
+        go.Bar(
+            x=panda_query[0],
+            y=panda_query[1],
+            name='Obitos por dia',
+            hovertemplate='Data: %{x}<br>Obitos: %{y}'           
+         ),
+        go.Scatter(
+            x=panda_query[0],
+            y=panda_query[2],
+            name='Média movel'
+        )
+    ]
+    layout = go.Layout(
+            xaxis=go.layout.XAxis(tickmode='auto')
+       
+    )
+    fig = go.Figure(data=data,layout=layout)
+    pagina = io.to_html(fig)
+    cursor.close()
+    connection.close()
+    return pagina
+
+
+@app.route("/casos_por_sintoma_geral")
+def casos_por_sintoma_geral():
+    connection = psycopg2.connect('postgresql://' + USUARIO_BD + ':' + SENHA_BD + '@192.168.0.100:5433/target_pb')
+    cursor = connection.cursor()
+    cursor.execute("select to_char(data_inicio_sintomas, 'YYYY-MM-DD'), casos_novos, casos_acumulados from casos_por_sintoma_geral")
+
+    result = cursor.fetchall()
+    panda_query = pd.DataFrame(list(result))
+
+    data = [
+        go.Bar(
+            x=panda_query[0],
+            y=panda_query[1],
+            name='Casos confirmados',
+            hovertemplate='Data: %{x}<br>Casos confirmados: %{y}'
         ),
-       go.Scatter(
-           x=panda_query[0],
-           y=panda_query[2],
-           name='Média movel'
-       )
-   ]
-   layout = go.Layout(
-           xaxis=go.layout.XAxis(tickmode='auto')
-      
-   )
-   fig = go.Figure(data=data,layout=layout)
-   pagina = io.to_html(fig)
-   cursor.close()
-   connection.close()
-   return pagina
+        go.Scatter(
+            x=panda_query[0],
+            y=panda_query[2],
+            name='Casos acumulados',
+            hovertemplate='Data: %{x}<br>Casos acumulados: %{y}'
+        )
+    ]
+    layout = go.Layout(xaxis=go.layout.XAxis(tickmode='auto'))
+
+    fig = go.Figure(data=data, layout=layout)
+    page = io.to_html(fig)
+    cursor.close()
+    connection.close()
+    return page
+
+@app.route("/obitos_por_ocorrencia_geral")
+def obitos_por_ocorrencia_geral():
+    connection = psycopg2.connect('postgresql://' + USUARIO_BD + ':' + SENHA_BD + '@192.168.0.100:5433/target_pb')
+    cursor = connection.cursor()
+    cursor.execute("select to_char(data_obitos, 'YYYY-MM-DD'), obitos_novos, obitos_acumulados from obitos_por_ocorrencia_geral")
+
+    result = cursor.fetchall()
+    panda_query = pd.DataFrame(list(result))
+
+    data = [
+        go.Bar(
+            x=panda_query[0],
+            y=panda_query[1],
+            name='Óbitos confirmados',
+            hovertemplate='Data: %{x}<br>Óbitos confirmados: %{y}'
+        ),
+        go.Scatter(
+            x=panda_query[0],
+            y=panda_query[2],
+            name='Óbitos acumulados',
+            hovertemplate='Data: %{x}<br>Óbitos acumulados: %{y}'
+        )
+    ]
+    layout = go.Layout(xaxis=go.layout.XAxis(tickmode='auto'))
+
+    fig = go.Figure(data=data, layout=layout)
+    page = io.to_html(fig)
+    cursor.close()
+    connection.close()
+    return page
