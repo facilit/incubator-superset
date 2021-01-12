ec51fb7e23f3412527b61b1a8817eb039d761fc9
Tiago Sousa <tsc2@cin.ufpe.br>
2021-01-12 11:00:14 -0300

    alterando configuracao de cookies do site

--- superset/config.py
+++ superset/config.py
@@ -842,7 +842,7 @@ ENABLE_ROW_LEVEL_SECURITY = False
 #
 SESSION_COOKIE_HTTPONLY = True  # Prevent cookie from being read by frontend JS?
 SESSION_COOKIE_SECURE = False  # Prevent cookie from being transmitted over non-tls?
-SESSION_COOKIE_SAMESITE = "Lax"  # One of [None, 'Lax', 'Strict']
+SESSION_COOKIE_SAMESITE = None  # One of [None, 'Lax', 'Strict']
 
 # Flask configuration variables
 SEND_FILE_MAX_AGE_DEFAULT = 60 * 60 * 24 * 365  # Cache static resources
